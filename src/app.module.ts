c4104c8db3ce6a402dd2c4d4dddaca8900022439
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-01 19:41:19 -0300

    Add global configuration module, database module, and users module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+import { DatabaseModule } from './database/database.module';
 import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        MONGODB_URI: Joi.string().required(),
+        PORT: Joi.number().required(),
+      }),
+      envFilePath: '../../.env',
+    }),
+    DatabaseModule,
+    PostsModule,
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
