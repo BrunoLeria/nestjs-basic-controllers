32b37257f7bf84bd112e909e8bf7fa0fd9bab13d
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-01 19:40:52 -0300

    Remove ConfigModule and DatabaseModule imports, and update MongooseModule import in Posts module

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,5 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
-import * as Joi from 'joi';
-import { DatabaseModule } from '../database/database.module';
 import { PostSchema, PostType } from './schemas/post.schema';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
@@ -10,15 +7,6 @@ import { PostsRepository } from './posts.repository';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      validationSchema: Joi.object({
-        MONGODB_URI: Joi.string().required(),
-        PORT: Joi.number().required(),
-      }),
-      envFilePath: '../../.env',
-    }),
-    DatabaseModule,
     MongooseModule.forFeature([{ name: PostType.name, schema: PostSchema }]),
   ],
   controllers: [PostsController],
