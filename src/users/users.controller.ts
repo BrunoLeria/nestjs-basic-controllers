c68b9a129134ea3e49f95f0d8641c0ff632ac0e0
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-01 20:34:32 -0300

    Add CRUD endpoints for users in UsersController

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,48 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserRequest } from './dto/create-users.request';
+import { UserType } from './schemas/user.schema';
+import { UpdateUserRequest } from './dto/update-users.request';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async createUser(
+    @Body() createUserRequest: CreateUserRequest,
+  ): Promise<UserType> {
+    return await this.usersService.createUser(createUserRequest);
+  }
+
+  @Get()
+  async getUsers(): Promise<UserType[]> {
+    return await this.usersService.getUsers();
+  }
+
+  @Get(':id')
+  async getUserById(@Param('id') id: string): Promise<UserType> {
+    return await this.usersService.getUserById(id);
+  }
+
+  @Patch(':id')
+  async updateUser(
+    @Param('id') id: string,
+    @Body() updateUserRequest: UpdateUserRequest,
+  ): Promise<UserType> {
+    return await this.usersService.updateUser(id, updateUserRequest);
+  }
+
+  @Delete(':id')
+  async deleteUser(@Param('id') id: string): Promise<{ deletedCount: number }> {
+    return await this.usersService.deleteUser(id);
+  }
+}
