a26a20e1474012cd8cac05d7ae06e4e46a6db223
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-01 16:57:32 -0300

    Add a new file which exports a function that returns a PostType object with preset values for _id, postId, content and title

---
+++ src/posts/test/stubs/post.stub.ts
@@ -0,0 +1,12 @@
+import { PostType } from 'src/posts/schemas/post.schema';
+import { Types } from 'mongoose';
+
+export const postStub = (): PostType => {
+  return {
+    _id: new Types.ObjectId('5f9d4a3d9d6c2c1f1c9d4408'),
+    postId: '1',
+    content:
+      'Exercitation id labore nostrud ex et adipisicing voluptate fugiat irure. Adipisicing elit elit amet commodo dolor. Irure pariatur qui consectetur ad nostrud sunt elit. Non sint incididunt sunt ut. Proident ea mollit dolore non irure voluptate occaecat.',
+    title: 'Quis quis incididunt commodo et est ea.',
+  };
+};
