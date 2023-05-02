600526eea45f5546eb0164298bf75c3651532105
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-02 14:12:40 -0300

    Update cookie-parser import to use namespace import in main.ts file. Add hot module replacement for development mode.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,9 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
-import cookieParser from 'cookie-parser';
+import * as cookieParser from 'cookie-parser';
+
+declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,5 +12,10 @@ async function bootstrap() {
   const configService = app.get(ConfigService);
   app.use(cookieParser());
   await app.listen(configService.get('PORT'));
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
