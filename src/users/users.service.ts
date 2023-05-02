68646b7af1039208c9b76f0bca238b22724b8e6c
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-02 02:57:56 -0300

    Add authentication service to UsersService constructor and add email validation to createUser method in UsersService

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,18 +1,44 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserRequest } from './dto/create-users.request';
 import { UpdateUserRequest } from './dto/update-users.request';
 import { UsersRepository } from './users.repository';
 import { UserType } from './schemas/user.schema';
 import { uuid } from 'uuidv4';
+import { AuthService } from '../auth/auth.service';
 @Injectable()
 export class UsersService {
-  constructor(private readonly usersRepository: UsersRepository) {}
+  constructor(
+    private readonly usersRepository: UsersRepository,
+    private readonly authService: AuthService,
+  ) {}
+
   async getUsers(): Promise<UserType[]> {
-    return await this.usersRepository.find({});
+    return this.usersRepository.find({});
   }
-  async getUserById(id: string): Promise<UserType> {
-    return await this.usersRepository.findOne({ userId: id });
+
+  async getUser(getUserArgs: Partial<UserType>) {
+    return this.usersRepository.findOne(getUserArgs);
   }
+
+  async createUser(createUserRequest: CreateUserRequest): Promise<UserType> {
+    if (await this.getUser({ email: createUserRequest.email })) {
+      throw new UnprocessableEntityException('Email already exists');
+    }
+    const session = await this.usersRepository.startTransaction();
+    try {
+      const newUser = { userId: uuid(), ...createUserRequest };
+      newUser.password = await this.authService.hashedPassword(
+        newUser.password,
+      );
+      const result = await this.usersRepository.create(newUser);
+      await session.commitTransaction();
+      return result;
+    } catch (err) {
+      await session.abortTransaction();
+      throw err;
+    }
+  }
+
   async updateUser(
     id: string,
     updateUserRequest: UpdateUserRequest,
@@ -30,18 +56,7 @@ export class UsersService {
       throw err;
     }
   }
-  async createUser(createUserRequest: CreateUserRequest): Promise<UserType> {
-    const session = await this.usersRepository.startTransaction();
-    try {
-      const newUser = { userId: uuid(), ...createUserRequest };
-      const result = await this.usersRepository.create(newUser);
-      await session.commitTransaction();
-      return result;
-    } catch (err) {
-      await session.abortTransaction();
-      throw err;
-    }
-  }
+
   async deleteUser(id: string): Promise<any> {
     const session = await this.usersRepository.startTransaction();
     try {
