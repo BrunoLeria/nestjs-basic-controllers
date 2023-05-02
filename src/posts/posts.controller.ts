29967db471e3f19e995bdbd6b715ae7178fb6b75
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-02 02:58:41 -0300

    Add JwtAuthGuard to protect post creation, editing, and deletion

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -6,17 +6,20 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreatePostRequest } from './dto/create-posts.request';
 import { PostType } from './schemas/post.schema';
 import { UpdatePostRequest } from './dto/update-posts.request';
+import JwtAuthGuard from '../auth/guards/jwt-auth.guard';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
   async createPost(
     @Body() createPostRequest: CreatePostRequest,
   ): Promise<PostType> {
@@ -29,11 +32,13 @@ export class PostsController {
   }
 
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   async getPostById(@Param('id') id: string): Promise<PostType> {
     return await this.postsService.getPostById(id);
   }
 
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   async updatePost(
     @Param('id') id: string,
     @Body() updatePostRequest: UpdatePostRequest,
@@ -42,6 +47,7 @@ export class PostsController {
   }
 
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   async deletePost(@Param('id') id: string): Promise<{ deletedCount: number }> {
     return await this.postsService.deletePost(id);
   }
