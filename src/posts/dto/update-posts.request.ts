4ca5f3da311e3330e231dc75ad9a2e595892253f
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-02 14:14:30 -0300

    Refactor class validators for update post and user requests

--- src/posts/dto/update-posts.request.ts
+++ src/posts/dto/update-posts.request.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePostRequest } from './create-posts.request';
-import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdatePostRequest extends PartialType(CreatePostRequest) {
-  @IsString()
-  @IsNotEmpty()
-  postId: string;
-}
+export class UpdatePostRequest extends PartialType(CreatePostRequest) {}
--- src/users/dto/update-users.request.ts
+++ src/users/dto/update-users.request.ts
@@ -1,9 +1,4 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserRequest } from './create-users.request';
-import { IsNotEmpty, IsString } from 'class-validator';
 
-export class UpdateUserRequest extends PartialType(CreateUserRequest) {
-  @IsString()
-  @IsNotEmpty()
-  userId: string;
-}
+export class UpdateUserRequest extends PartialType(CreateUserRequest) {}
