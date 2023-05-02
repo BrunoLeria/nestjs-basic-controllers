b7c19729eda30e762c52c8251dcb0771910e9125
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-02 17:53:36 -0300

    Add logout route and method with JwtAuthGuard to AuthController

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,17 @@
-import { Controller, HttpCode, Post, UseGuards, Res } from '@nestjs/common';
+import {
+  Controller,
+  HttpCode,
+  Post,
+  UseGuards,
+  Res,
+  Get,
+} from '@nestjs/common';
 import { CurrentUser } from './current-user.decorator';
 import { LocalAuthGuard } from './guards/local-auth.guard';
 import { Response } from 'express';
 import { UserType } from '../users/schemas/user.schema';
 import { AuthService } from './auth.service';
+import JwtAuthGuard from './guards/jwt-auth.guard';
 
 @Controller('login')
 export class AuthController {
@@ -18,5 +26,12 @@ export class AuthController {
   ) {
     await this.authService.login(user, response);
     response.send(user);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get()
+  async logout(@Res({ passthrough: true }) response: Response) {
+    await this.authService.logout(response);
+    response.send();
   }
 }
