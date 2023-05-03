33ff214c585b2a990656994aa923abea68b3a284
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-03 13:31:44 -0300

    Add mock services for auth, config, express, jwt, and users in present tense

---
+++ src/auth/__mocks__/auth.service.ts
@@ -0,0 +1,5 @@
+import { userStub } from '../../users/test/stubs/user.stub';
+export const AuthService = jest.fn().mockReturnValue({
+  login: jest.fn().mockResolvedValue(userStub()),
+  logout: jest.fn().mockResolvedValue(''),
+});
