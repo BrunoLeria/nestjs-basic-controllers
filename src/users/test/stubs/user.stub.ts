c4b4b2a416f03b7d0c5d45d27fdfa5858f0be308
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-04 17:14:32 -0300

    Update user.stub.ts to use UUID for userId and generic password

--- src/users/test/stubs/user.stub.ts
+++ src/users/test/stubs/user.stub.ts
@@ -4,9 +4,9 @@ import { Types } from 'mongoose';
 export const userStub = (): UserType => {
   return {
     _id: new Types.ObjectId('5f9d4a3d9d6c2c1f1c9d4408'),
-    userId: '1',
+    userId: '077959d4-3cbf-458a-ad61-742cbb3b7b99',
     email: 'michael.lawson@reqres.in',
     name: 'Michael Lawson',
-    password: '$2b$10$isaOSpX9mUZ7PKi0SThkSujtUqrk4iaNh9zfiyZz2ocEOTj2QlGKW',
+    password: 'password',
   };
 };
