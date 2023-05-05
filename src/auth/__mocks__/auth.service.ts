096d62b9a93271b39f73f1b0450a1f018b34246f
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-05 02:19:56 -0300

    Add validation method to mock AuthService for testing purposes

--- src/auth/__mocks__/auth.service.ts
+++ src/auth/__mocks__/auth.service.ts
@@ -2,4 +2,5 @@ import { userStub } from '../../users/test/stubs/user.stub';
 export const AuthService = jest.fn().mockReturnValue({
   login: jest.fn().mockResolvedValue(userStub()),
   logout: jest.fn().mockResolvedValue(''),
+  validateUser: jest.fn().mockResolvedValue(userStub()),
 });
